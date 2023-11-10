8e68e1b9ad4da7cb249a8afd77bd1b3e7e42539f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 12:55:16 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,40 +1,33 @@
+import { useEffect, useState, useRef } from "react"
+import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// import axios from "./api/axios"
 
-import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfocircle } from "@fortawesome/free-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "./api/axios";
-import { fa0 } from "@fortawesome/free-solid-svg-icons";
-
-
-
-const USER_REGEX = /^[A-Z][a-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL = "/register";
-
-
+const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
+const REGISTER_URL = "/register"
 
 const Register = () => {
-    const userRef = useRef();
-    const errRef = useRef();
+    const userRef = useRef()
+    const errRef = useRef()
 
-    const [ user, setUser ] = useState()
-    const [ validName, setValidName ] = useState(false)
-    const [ userFocus, setUserFocus ] = useState(false);
+    const [user, setUser] = useState("")
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
 
-    const [ pwd, setPwd ] = useState('');
-    const [ validPwd, setValidPwd ] = useState(false)
-    const [ pwdFocus, setPwdFocus ] = useState(false)
+    const [pwd, setPwd] = useState('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
 
-    const [ matchPwd, setMatchPwd ] = useState('')
-    const [ validMatch, setValidMatch ] = useState(false)
-    const [ matchFocus, setMatchFocus ] = useState(false)
+    const [matchPwd, setMatchPwd] = useState("")
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
 
-    const [ errMsg, setErrMsg ] = useState('')
-    const [ success, setSuccess ] = useState(false)
-
-
+    const [errMsg, setErrMsg] = useState("")
+    const [success, setSuccess] = useState(false)
+  
     useEffect(() => {
-        useRef.current.focus()
+        userRef.current.focus()
     }, [])
 
     useEffect(() => {
@@ -48,63 +41,61 @@ const Register = () => {
     useEffect(() => {
         setErrMsg("")
     }, [user, pwd, matchPwd])
-
+    
 
   return (
     <>
-    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}> {errMsg} </p>
-
-    <form>
-        <label htmlFor="username">
-            username: 
-            <FontAwesomeIcon  icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"}/>
-            <FontAwesomeIcon  icon={faTimes} className={validMatch || matchPwd ? "hide" : "invalid"}/>
-        </label>
-
-        <input type="text" 
-                id="username" 
-                ref={userRef} 
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <form>
+            <label htmlFor="username">
+                username:
+                <FontAwesomeIcon icon={faCheck} className = {validMatch && matchPwd ? "valid" : "hide"} />
+                <FontAwesomeIcon icon={faTimes} className = {validMatch || matchPwd ? "hide" : "invalid"} />
+            </label>
+            <input 
+                type="text" 
+                id="username"
+                ref={userRef}
                 autoComplete="off"
-                onChange={(e) => setUser(e.target.value)} 
+                onChange={(e) => setUser(e.target.value)}
                 value={user}
                 required
-                aria-invalid={validName ? "false" : "true" }
+                aria-invalid={validName ? "false" : "true"}
                 aria-describedby="uidnote"
                 onFocus={() => setUserFocus(true)}
                 onBlur={() => setUserFocus(false)}
-                />
+            />
 
-                <p id="uidnote" className={userFocus && user && !validName ? "instruction" : "offscreen"}>
-                    <FontAwesomeIcon icon={faInfocircle} />
-                    3 to 23 Characters 
-                         Must beging <br /> with a 
-                         letter, number, <br /> underscore, hyphens.
-                </p>
+            <p id="uidnote" className={userFocus && user && !validName ? "instruction" : "offscreen"}>
+                <FontAwesomeIcon icon={faInfoCircle}/>
+                3 to 23 characters <br /> 
+                Must begin with a letter, number, underscore, hyphens allowed
+            </p>
 
-        <label htmlFor="password">
-            password: 
-            <FontAwesomeIcon  icon={faCheck} className={validPwd ? "valid" : "hide"}/>
-            <FontAwesomeIcon  icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"}/>
-        </label>
-
-        <input type="password" 
-                id="password" 
-                onChange={(e) => setPwd(e.target.value)} 
+            <label htmlFor="password">
+                password:
+                <FontAwesomeIcon icon={faCheck} className = {validPwd ? "valid" : "hide"} />
+                <FontAwesomeIcon icon={faTimes} className = {validPwd || !pwd ? "hide" : "invalid"} />
+            </label>
+            <input 
+                type="password" 
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
                 value={pwd}
                 required
-                aria-invalid={validPwd ? "false" : "true" }
+                aria-invalid={validPwd ? "false" : "true"}
                 aria-describedby="uidnote"
                 onFocus={() => setPwdFocus(true)}
                 onBlur={() => setPwdFocus(false)}
-                />
+            />
 
-                    <FontAwesomeIcon icon={faInfocircle} />
-                    8 to 24 Characters 
-                    Must Include UpperCase And <br /> LowerCase Letters.
-                    a number and special character. <br />
-                        Allowed Special Character:
-                
-    </form>
+            <p>
+                <FontAwesomeIcon icon={faInfoCircle}/>
+                8 to 24 characters <br /> 
+                Must include Uppercase and LowerCase Letters, a number and a special character <br />
+                Allowed special character
+            </p>
+        </form>
     </>
   )
 }
