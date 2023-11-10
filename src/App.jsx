052a4fcf4a6279134dbda5a779a4120daa2d3558
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 11:29:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Register from "./Register"
+
+
+
+
+
+const App = () => {
+  return (
+    <main className='App'>
+      <Register />
+    </main>
+  )
+}
+
+export default App
---
+++ src/Register.jsx
@@ -0,0 +1,68 @@
+
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfocircle } from "@fortawesome/free-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "./api/axios";
+import { fa0 } from "@fortawesome/free-solid-svg-icons";
+
+
+
+const USER_REGEX = /^[A-Z][a-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = "/register";
+
+
+
+const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [ user, setUser ] = useState()
+    const [ validName, setValidName ] = useState(false)
+    const [ userFocus, setUserFocus ] = useState(false);
+
+    const [ pwd, setPwd ] = useState('');
+    const [ validPwd, setValidPwd ] = useState(false)
+    const [ pwdFocus, setPwdFocus ] = useState(false)
+
+    const [ matchPwd, setMatchPwd ] = useState('')
+    const [ validMatch, setValidMatch ] = useState(false)
+    const [ matchFocus, setMatchFocus ] = useState(false)
+
+    const [ errMsg, setErrMsg ] = useState('')
+    const [ success, setSuccess ] = useState(false)
+
+
+    useEffect(() => {
+        useRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setValidName(USER_REGEX.test(user))
+    }, [user])
+
+    useEffect(() => {
+        setValidPwd(PWD_REGEX.test(pwd))
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg("")
+    }, [user, pwd, matchPwd])
+
+
+  return (
+    <>
+    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}> {errMsg} </p>
+
+    <form>
+        <label htmlFor="username">
+            username: 
+            <FontAwesomeIcon  icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"}/>
+            <FontAwesomeIcon  icon={faTimes} className={validMatch || matchPwd ? "hide" : "invalid"}/>
+        </label>
+    </form>
+    </>
+  )
+}
+
+export default Register
