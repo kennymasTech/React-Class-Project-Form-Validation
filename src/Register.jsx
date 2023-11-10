ef98478e77f4933f66401e3606f6201909b65415
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 13:14:15 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -9,6 +9,8 @@ const USER_REGEX = /^[A-Z][A-Z0-9-_]{3,23}$/
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const REGISTER_URL = "/register"
 
+
+
 const Register = () => {
     const userRef = useRef()
     const errRef = useRef()
