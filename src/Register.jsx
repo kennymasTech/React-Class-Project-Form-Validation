e5520a95e50a3b4c2d55cd674a0dff34724793a5
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 13:14:39 +0100

    update

--- src/Register.jsx
+++ src/Register.jsx
@@ -30,6 +30,7 @@ const Register = () => {
     const [errMsg, setErrMsg] = useState("")
     const [success, setSuccess] = useState(false)
   
+    
     useEffect(() => {
         userRef.current.focus()
     }, [])
