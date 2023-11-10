60cc234421ced03f11f4cc6b2fac19b69c72f969
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 23:17:57 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState, useRef } from "react";
 import {faCheck, faTimes, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 
