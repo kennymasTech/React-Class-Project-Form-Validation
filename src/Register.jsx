73a5a6585bc2a5f31d68a3958c1108eef799961a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-10 23:20:28 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     const [validPwd, setValidPwd] = useState(false);
     const [pwdFocus, setPwdFocus] = useState(false);
 
-    const [matchPwd, setMatchPwd] = useState("")
+    const [matchPwd, setMatchPwd] = useState("");
     const [validMatch, setValidMatch] = useState(false)
     const [matchFocus, setMatchFocus] = useState(false)
 
