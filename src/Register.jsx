fdb3b595fcfd7ca380e06620b53e2d6ee147a015
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-12 20:03:58 +0100

    a

--- src/Register.jsx
+++ src/Register.jsx
@@ -28,7 +28,7 @@ const Register = () => {
     const [matchFocus, setMatchFocus] = useState(false);
 
     const [errMsg, setErrMsg] = useState("");
-    const [success, setSuccess] = useState(false);
+    const [success, setSuccess] = useState(false)
   
 
     useEffect(() => {
